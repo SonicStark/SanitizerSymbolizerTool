a9a7cdf8a0483b2c90373c42426fb7fe975aca9b
SonicStark <50692172+SonicStark@users.noreply.github.com>
2023-05-13 14:58:34 +0000

    public interface

---
+++ include/sanitizer_symbolizer_tool.h
@@ -0,0 +1,14 @@
+#ifndef SANSYMTOOL_HEAD_PUBLIC_INTERFACE_H
+#define SANSYMTOOL_HEAD_PUBLIC_INTERFACE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif
