1e22fb5db839435fa20da74a8509b2d55125a5df
SonicStark <50692172+SonicStark@users.noreply.github.com>
2023-05-18 15:34:51 +0800

    build script for simple_demo

---
+++ demo/simple_demo_dummy_build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DIR_CUR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" #https://stackoverflow.com/a/4774063
+DIR_LIB=`dirname $DIR_CUR`/lib
+DIR_INC=`dirname $DIR_CUR`/include
+
+COMMON_FLAG="-g -O0 -fsanitize=address -I$DIR_INC"
+
+$CC  $COMMON_FLAG -c $DIR_CUR/simple_demo.c           -o $DIR_CUR/demo-main-tmp.o
+$CXX $COMMON_FLAG -c $DIR_LIB/interface.cpp           -o $DIR_CUR/demo-interface-tmp.o
+$CXX $COMMON_FLAG -c $DIR_LIB/common.cpp              -o $DIR_CUR/demo-common-tmp.o
+$CXX $COMMON_FLAG -c $DIR_LIB/symbolizer.cpp          -o $DIR_CUR/demo-symbolizer-tmp.o
+$CXX $COMMON_FLAG -c $DIR_LIB/use_llvm_symbolizer.cpp -o $DIR_CUR/demo-llvm-tmp.o
+$CXX $COMMON_FLAG -c $DIR_LIB/use_addr2line.cpp       -o $DIR_CUR/demo-ad2l-tmp.o
+
+$CXX $COMMON_FLAG \
+        $DIR_CUR/demo-main-tmp.o \
+        $DIR_CUR/demo-interface-tmp.o \
+        $DIR_CUR/demo-common-tmp.o \
+        $DIR_CUR/demo-symbolizer-tmp.o \
+        $DIR_CUR/demo-llvm-tmp.o \
+        $DIR_CUR/demo-ad2l-tmp.o \
+-o $DIR_CUR/simple_demo
+
+rm -f $DIR_CUR/demo-*-tmp.o
+
+echo "ALL DONE"
